b9c80a6f82a9b6c304b3b15a669008e886ca0ba2
Jordan Dinwiddy <jdinwiddy@JDINW1ML1.local>
2013-03-05 20:53:26 -0800

    Adding example servlet, main class and config

---
+++ src/main/java/com/dinwiddy/examples/embedded_jetty/ServerBootstrap.java
@@ -0,0 +1,38 @@
+package com.dinwiddy.examples.embedded_jetty;
+
+import java.net.URL;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class ServerBootstrap {
+
+	private static final int DEFAULT_SERVER_PORT = 8080;
+
+	public static void main(String[] args) throws Exception {
+		
+		int serverPort = DEFAULT_SERVER_PORT;
+		if(args.length == 1) {
+			serverPort = Integer.parseInt(args[0]);
+		}
+		
+		System.out.println("Running Jetty server on port " + serverPort);
+		
+		Server server = new Server(8080);
+		WebAppContext root = new WebAppContext();
+	    
+	    final URL webappUrl = ServerBootstrap.class.getClassLoader().getResource("webapp");
+	    final String webappUrlStr = webappUrl.toExternalForm();
+	    
+		root.setContextPath("/");
+		root.setDescriptor(webappUrlStr + "WEB-INF/web.xml");
+		root.setResourceBase(webappUrlStr);
+
+		root.setParentLoaderPriority(true);
+
+		server.setHandler(root);
+
+		server.start();
+		server.join();
+	}
+}
